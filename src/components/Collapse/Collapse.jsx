1f51e482808cf15767499ccd193a53ab5f56c865
Mathieu Bouvet <mathieubouvet42@gmail.com>
2021-12-02 18:55:16 +0100

    Add collapsible messages on about page

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import cx from "../../utils/cx";
+
+import styles from "./collapse.module.scss";
+
+class Collapse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpened: false,
+    };
+  }
+
+  toggle = () => {
+    this.setState(state => ({
+      ...state,
+      isOpened: !state.isOpened,
+    }));
+  };
+
+  render() {
+    return (
+      <div className={cx(styles.collapse, this.props.className)}>
+        <button className={styles.trigger} onClick={this.toggle}>
+          {this.props.title}
+          <img
+            src="./chevron-up.svg"
+            alt={`chevron vers le ${this.state.isOpened ? "bas" : "haut"}`}
+            className={cx(styles.chevron, {
+              [styles.down]: this.state.isOpened,
+            })}
+          />
+        </button>
+        <div
+          className={cx(styles.content, {
+            [styles.opened]: this.state.isOpened,
+          })}
+        >
+          <div className={styles.wrapper}>{this.props.children}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Collapse.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.string,
+  className: PropTypes.string,
+};
+
+export default Collapse;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 
 import Hero from "../../components/Hero";
+import Collapse from "../../components/Collapse";
+
+import styles from "./about.module.scss";
 
 class About extends React.Component {
   render() {
     return (
       <>
         <Hero background="./images/background-about.jpg"></Hero>
+        <Collapse title="Fiabilité" className={styles.collpasibleMessage}>
+          Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+          photos sont conformes aux logements, et toutes les informations sont
+          régulièrement vérifiées par nos équipes.
+        </Collapse>
+        <Collapse title="Respect" className={styles.collpasibleMessage}>
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </Collapse>
+        <Collapse title="Service" className={styles.collpasibleMessage}>
+          Nos équipes se tiennent à votre disposition pour vous fournir une
+          expérience parfaite. N'hésitez pas à nous contacter si vous avez la
+          moindre question.
+        </Collapse>
+        <Collapse title="Sécurité" className={styles.collpasibleMessage}>
+          La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+          pour les voyageurs, chaque logement correspond aux critères de
+          sécurité établis par nos services. En laissant une note aussi bien à
+          l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les
+          standards sont bien respectés. Nous organisons également des ateliers
+          sur la sécurité domestique pour nos hôtes.
+        </Collapse>
       </>
     );
   }
